39e0bb40199396e5d6132ee313175284f20dbdd1
Jim Zhou <jim@vellicle.com>
2016-06-27 04:04:09 -0400

    Delete stub.sh

--- stub.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-apt-get -y update
-apt-get -y install screen
-wget https://raw.github.com/XertroV/BitcoinAutoNode/master/bitcoinAutoNode.sh
-screen -dmS bitcoinInstaller sudo bash bitcoinAutoNode.sh
